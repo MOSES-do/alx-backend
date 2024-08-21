fb8ee372616aafcd9f0410b9ff2ecb0a9007aed7
MOSES-do <archer.me24@gmail.com>
2024-08-21 15:33:59 +0000

    Extending a simple base class cache system

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,16 +6,14 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-  """Representation of a Cache System"""
-  def put(self, key, item):
-    """Add items to cache dictionary"""
-    if key is not None or item is not None:
-      self.cache_data[key] = item
+    """Representation of a Cache System"""
+    def put(self, key, item):
+        """Add items to cache dictionary"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
 
-
-  def get(self, key):
-    """Retrieve items from cache dict"""
-    if key is None or key not in self.cache_data:
-      return None
-    return self.cache_data.get(key)
-
+    def get(self, key):
+        """Retrieve items from cache dict"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
