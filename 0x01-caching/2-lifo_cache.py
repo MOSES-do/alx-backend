4abd1c9eb8350281f24cd47fe163cb645014b3bc
MOSES-do <archer.me24@gmail.com>
2024-08-22 03:14:13 +0000

    LIFO Caching system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" LIFO Caching System"""
+
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """Object that stroes and retrieves items from
+       a dictionary utilizing the FIFO removal mechanism
+       when the cache storage limit is reached.
+    """
+    def __init__(self):
+        """initialize derived class"""
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item to cache"""
+        if key is None or item is None:
+            return
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                last_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", last_key)
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """Retrieves an item based on its key."""
+        return self.cache_data.get(key, None)
