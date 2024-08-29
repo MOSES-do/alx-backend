393a2448ca786da913e7168be0ad4bbbfa99b8da
MOSES-do <archer.me24@gmail.com>
2024-08-29 13:09:56 +0000

    translations update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -19,7 +19,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel_localeselector
+@babel.localeselector
 def get_locale():
     """Returns best match of supported language"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
