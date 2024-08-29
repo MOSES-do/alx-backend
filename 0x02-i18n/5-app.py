ef84f16d9473e0efa6ba234344fba114ed2d5f27
MOSES-do <archer.me24@gmail.com>
2024-08-29 14:14:02 +0000

    simulate user login verification

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Setup Babel functionality in our flask app"""
 from flask_babel import Babel
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, g
 app = Flask(__name__)
 
 
