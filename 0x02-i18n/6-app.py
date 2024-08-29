8c74c8774234a31f889048fed1c8e448ce1ae833
MOSES-do <archer.me24@gmail.com>
2024-08-29 14:47:34 +0000

    user locale

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""Setup Babel functionality in our flask app"""
+from flask_babel import Babel
+from flask import Flask, render_template, request, g
+app = Flask(__name__)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config:
+    """Int'l Config for our app"""
+
+    """Babel's default locale/utc"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Locale from URL parameters"""
+    locale = request.args.get('locale', '')
+    if locale in app.config["LANGUAGES"]:
+        return locale
+    """Locale from user settings"""
+    if g.user and g.user['locale'] in app.config["LANGUAGES"]:
+        return g.user['locale']
+    """Locale from request header"""
+    header_locale = request.headers.get('locale', '')
+    if header_locale in app.config["LANGUAGES"]:
+        return header_locale
+    """Default locale"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.before_request
+def before_request() -> None:
+    """carries out routines before each request's resolution.
+    """
+    user = get_user()
+    g.user = user
+
+
+def get_user():
+    """Retrieves a user based on a user id.
+    """
+    login_id = request.args.get('login_as')
+    if login_id:
+        return users.get(int(login_id))
+    return None
+
+
+@app.route("/")
+def homepage():
+    """Sites homepage"""
+
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
