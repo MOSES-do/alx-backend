7250bd736d92636397800ba5c7b536e58a6e2593
MOSES-do <archer.me24@gmail.com>
2024-08-29 11:08:47 +0000

    babel.localeselector - determine best match with supported languages

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Setup Babel functionality in our flask app"""
+from flask_babel import Babel
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+class Config:
+    """Int'l Config for our app"""
+
+    """Babel's default locale/utc"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+"""Treat same word routes the same regardless of an ending /"""
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel_localeselector
+def get_locale:i
+    """Returns best match of supported language"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def homepage():
+    ""Sites homepage"""
+
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x02-i18n/templates/2-index.html
@@ -0,0 +1,7 @@
+<! DOCTYPE html>
+<html>
+<body>
+	<title>Welcome to Holberton</title>
+	<header><h1>Hello world</h1></header>
+</body>
+</html>
