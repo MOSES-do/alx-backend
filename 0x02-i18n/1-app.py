55f3bd602697979d9710058a6222c3f7265869b4
MOSES-do <archer.me24@gmail.com>
2024-08-29 10:55:55 +0000

    Set Babel's locale/timezone

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Setup Babel functionality in our app"""
+"""Setup Babel functionality in our flask app"""
 from flask import Flask, render_template
 from flask_babel import Babel
 app = Flask(__name__)
