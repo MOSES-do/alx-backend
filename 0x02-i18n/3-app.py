5f7782636fa1eca25b6595ded1f37071575ad058
MOSES-do <archer.me24@gmail.com>
2024-08-29 13:04:51 +0000

    translations update

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Setup Babel functionality in our flask app"""
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
